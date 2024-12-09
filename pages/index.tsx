ab05cbe46811a9541f39549332aaef4182117628
Rômulo Guimarães <romulo1984@gmail.com>
2024-12-09 20:11:29 +0000

    added about page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,16 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.scss'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.scss";
+import Image from "next/image";
 
-const APP_URL = process.env.NEXT_PUBLIC_APP_URL
+const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
 
-interface IHomeProps {
-  name: string
-  appName?: string
-  message?: string
-}
-
-const Home: React.FC<IHomeProps | NextPage> = ({ message }: IHomeProps) => {
+const Home: React.FC<NextPage> = () => {
   return (
     <div className={styles.container}>
-      <Image src="/dibre-logo.svg" width={200} height={40}/>
-      <p>{message}</p>
+      <Image src="/dibre-logo.svg" width={200} height={40} />
+      <p>Em breve você vai dibrar todo mundo!</p>
     </div>
-  )
-}
+  );
+};
 
-export async function getStaticProps () {
-  const res = await fetch(`${APP_URL}/api/hello`)
-  const hello = await res.json()
-  
-  return {
-    props: {
-      message: hello.message || 'Hello World'
-    }
-  }
-}
-
-export default Home
+export default Home;
